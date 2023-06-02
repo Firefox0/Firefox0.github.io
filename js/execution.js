b9d585df955f992f56c83339936f080fde4bc34f
Firefox0 <you@example.com>
2023-06-02 10:00:30 +0200

    Refactoring

--- js/execution.js
+++ js/execution.js
@@ -1,19 +1,58 @@
+import Ultimate from "./ultimate.js";
+import Timer from "./timer.js";
+import Score from "./score.js";
+import Hp from "./hp.js";
+import Explanation from "./explanation.js";
+import Title from "./title.js";
+import Settings from "./settings.js";
+
 export default class Execution {
     static yesButton = document.getElementById("yes");
     static noButton = document.getElementById("no");
+
+    static {
+        this.initializeButtons();
+    }
+
+    static progress(answer) {
+        let ultDamage = Ultimate.calculateUltDamage(Hp.currentHp, Hp.maximumHp);
+        Timer.stopTimer();
+        Timer.increaseOffset(0.25);
+        if ((ultDamage >= Hp.currentHp) === answer) {
+            Score.updateScore(Score.currentScore + 1)
+            this.newGame();
+        } else {
+            this.gameOver(ultDamage);
+        }
+    }
+
+    static newGame() {
+        Title.refreshTitle(Settings.getDifficulty());
+        Ultimate.randomizeUltLevel();
+        Hp.newHealth(Settings.getDifficulty());
+
+        Timer.resetTimer();
+        Timer.startTimer(() => this.gameOver(Ultimate.calculateUltDamage(Hp.currentHp, Hp.maximumHp)));
+    }
+
+    static gameOver(ultDamage) {
+        Explanation.showExplanation(ultDamage);
+        Explanation.toggleUI();
+        Execution.toggleUI();
+    }
 
     static initializeButtons(callback) {
         this.yesButton.onclick = () => {
             if (this.yesButton.hasAttribute("disabled")) {
                 return;
             }
-            callback(true);
+            this.progress(true);
         }
         this.noButton.onclick = () => {
             if (this.noButton.hasAttribute("disabled")) {
                 return;
             }
-            callback(false);
+            this.progress(false);
         }
     }
 
