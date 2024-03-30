04fd578050706df02741035c001b6d8207047b0f
Firefox0 <you@example.com>
2024-03-30 15:30:49 +0100

    Max is now inclusive

--- src/random.ts
+++ src/random.ts
@@ -1,5 +1,5 @@
 export function getRandomInt(min: number, max: number): number {
-    return Math.floor(min + Math.random() * (max - min));
+    return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
 export function coinflip(): boolean {
