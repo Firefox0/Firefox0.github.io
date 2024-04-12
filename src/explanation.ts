578374737e8c6eb93254a1d523b2aca2ffaf77d9
Firefox0 <you@example.com>
2024-04-12 13:46:35 +0200

    UI Improvements

--- src/explanation.ts
+++ src/explanation.ts
@@ -1,7 +1,7 @@
 export function getExplanation(currentHp: number, maximumHp: number, ultDamage: number): string {
-    return `Correct Answer: ${(ultDamage >= currentHp) ? "👍" : "👎"} <br>
-            Maximum HP: ${Math.trunc(maximumHp)} <br>
-            Current HP: ${currentHp} <br>
-            Ult Damage: ${Math.trunc(ultDamage)} <br>
-            Remaining HP: ${Math.trunc(currentHp - ultDamage)}`
+    return `CORRECT ANSWER: ${(ultDamage >= currentHp) ? "👍" : "👎"} <br>
+            MAXIMUM HP: ${Math.trunc(maximumHp)} <br>
+            CURRENT HP: ${currentHp} <br>
+            ULT DAMAGE: ${Math.trunc(ultDamage)} <br>
+            REMAINING HP: ${Math.trunc(currentHp - ultDamage)}`
 }
